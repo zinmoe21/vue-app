18ad6aa69c220c1489a57a87a5f03f9320659b77
Zin Moe <zinmoe@sys-city.com>
2019-09-25 16:23:11 +0630

    CRUD with async

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,121 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="small-container">
+    <h1>Employees</h1>
+
+    <employee-form 
+      @add:employee='addEmployee'
+    />
+    <employee-table 
+      :employees='employees' 
+      @delete:employee='deleteEmployee'
+      @edit:employee='editEmployee'
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EmployeeTable from '@/components/EmployeeTable.vue'
+import EmployeeForm from '@/components/EmployeeForm.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    EmployeeTable,
+    EmployeeForm
+  },
+  data() {
+    return {
+      employees: [],
+    }
+  },
+
+  mounted() {
+    this.getEmployees()
+  },
+
+  methods: {
+
+    async getEmployees() {
+      try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/users`)
+        const data = await response.json()
+        this.employees = data
+      } catch (error) {
+        console.error(error)
+      }
+    },
+
+    async addEmployee(employee) {
+      try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/users`, {
+          method: 'POST',
+          body: JSON.stringify(employee),
+          headers: { 'Content-type': 'application/json; charset=UTF-8' },
+        })
+
+        const data = await response.json()
+        this.employees = [...this.employees, data]
+      } catch (error) {
+        console.error(error)
+      }
+    },
+
+    async editEmployee(id, updatedEmployee) {
+      try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+          method: 'PUT',
+          body: JSON.stringify(updatedEmployee),
+          headers: {'Content-type': 'application/json; charset=UTF-8'},
+        })
+        const data = await response.json()
+        this.employees = this.employees.map(employees => (employee.id === id ? data : employee))
+      } catch (error) {
+        consloe.error(error)
+      }
+    },
+
+    async deleteEmployee(id) {
+      try {
+        await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+          method: 'DELETE'
+        });
+        this.employees = this.employees.filter(employee => employee.id !== id);
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    // addEmployee(employee) {
+    //   const lastId =
+    //     this.employees.length > 0
+    //       ? this.employees[this.employees.length - 1].id
+    //       : 0;
+    //   const id = lastId + 1;
+    //   const newEmployee = { ...employee, id };
+
+    //   this.employees = [...this.employees, newEmployee];
+    // },
+    // deleteEmployee(id) {
+    //   this.employees = this.employees.filter(
+    //       employee => employee.id !== id
+    //     )
+    // },
+    // editEmployee(id, updatedEmployee) {
+    //   this.employees = this.employees.map( employee => 
+    //     employee.id === id ? updatedEmployee : employee
+    //   )
+    // }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  button {
+    background: #009435;
+    border: 1px solid #009435;
+  }
+
+  .small-container {
+    max-width: 720px;
+  }
 </style>
